78bcae8a2a22ed1f1664a8826a7f9fe44ce00f3a
penguin <scokro7141@gmail.com>
2025-01-05 19:42:35 -0500

    Added quality-of-life features to creating circuits

--- src/manim_circuit/utils.py
+++ src/manim_circuit/utils.py
@@ -67,29 +67,6 @@ class Source(VMobject):
         return self
 
 
-def distance(a, b):
-    return np.sqrt(np.sum([i * i for i in np.array(a) - np.array(b)]))
-
-
-def validate_line_intersection(l1, l2):
-    tolerance = 1e-5
-    if math.isclose(
-        distance(l2[0], l1[0]) + distance(l2[0], l1[1]),
-        distance(*l1),
-        rel_tol=tolerance,
-    ) or math.isclose(
-        distance(l2[1], l1[0]) + distance(l2[1], l1[1]),
-        distance(*l1),
-        rel_tol=tolerance,
-    ):
-        try:
-            return line_intersection(l1, l2)
-        except:
-            return False
-    else:
-        return False
-
-
 class Circuit(VMobject):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -103,122 +80,208 @@ class Circuit(VMobject):
         self.add(self.component_list)
 
         # Add node_list VGroup() of nodes (with wires)
-        self.add(self.node_list)
         # Add Dot() for junctions
+        self.add(self.node_list)
+
+    # This function returns the endpoints of the wire.
+    def __create_wire(self, end1, end2, diagonal=False, invert=False):
+        # Check if a turn is necessary. Only satisfiable if:
+        # 1. diagonal flag is not set/overriden to True
+        # 2. end1.x != end2.x and end1.y != end2.y
+        if (
+            not math.isclose(end1[0], end2[0]) and not math.isclose(end1[1], end2[1])
+        ) and diagonal is not True:
+            # Define the turn
+            if invert is True:
+                turn = [end2[0], end1[1], 0]
+            else:
+                turn = [end1[0], end2[1], 0]
+            return [end1, turn, end2]
+
+        # This is diagonal, OR it is a straight line
+        else:
+            return [end1, end2]
 
     def add_components(self, *args):
         for component in args:
             self.component_list.add(component)
 
-    def add_wire(self, end1, end2, junctions=True):
+    def add_wire(
+        self,
+        end1,
+        end2,
+        diagonal=False,
+        invert=False,
+    ):
+        wire = self.__create_wire(end1, end2, diagonal, invert)
+
         # First wire
         if len(self.node_list) == 0:
-            newNode = Node()
-            newWire = Wire(end1, end2)
-            newNode.add_wire_to_node(newWire)
-            self.node_list.add(newNode)
-            return
+            # Create the first node
+            node = Node()
+            node.add_wire(wire)
+
+            # Adding the newly editted node
+            self.node_list.add(node)
+
+            return wire
         # Not first wire.
         # Loop through all nodes and wires through each node
         else:
             intersections = []
-            wires = []
-            node_attachments = []
-
-            # NOTE type(node) == Node
+            # Iterate through all nodes.
             for node in self.node_list:
-                # NOTE type(wire) == VMobject
-                for wire in node.line_wires:
-                    intersect = validate_line_intersection(
-                        wire.get_anchors(),
-                        [end1, end2],
-                    )
+                # search if a coordinate of the wire belong in the node.
+                # If so, add to node intersections
+                # search is a list that returns either True or coordinate or False
+                # It is important because we can then add Dots() from this list.
+                search = list(map(node.check_coord, wire))
+                if not all([type(s) == bool and s == False for s in search]):
+                    intersections.append(node)
 
-                    # Found intersection.
-                    if not intersect is False:
-                        wires.append(wire)
-                        intersections.append(intersect)
-                        node_attachments.append(node)
+                    # Go through the search
+                    # If there is a coordinate, tell the node to add a junction there.
+                    for dot in search:
+                        if not type(dot) == bool:
+                            node.add_dot(dot)
 
-            # No intersections means new node.
+
+            # This means that the wire is not attached to any node,
+            # Make a new node.
             if len(intersections) == 0:
-                newNode = Node()
-                newWire = Wire(end1, end2)
+                node = Node()
+                node.add_wire(wire)
+                node.set_color(WHITE)
+                self.node_list.add(node)
 
-                # the add_wire method will automatically let the Wire object
-                # know what its root is (which is just gonna be the Node itself)
-                newNode.add_wire_to_node(newWire)
-
-                self.node_list.add(newNode)
-
-            # Seems to intersect at one wire
-            # just add this new wire onto the Wire() root.
             elif len(intersections) == 1:
-                newWire = Wire(end1, end2)
-                node_attachments[0].add_wire_to_node(newWire)
-
-                if junctions is True:
-                    node_attachments[0].add_junction(intersections[0])
+                intersections[0].add_wire(wire)
 
             elif len(intersections) == 2:
-                # Merge downwards from 1 to 0
-                # Collect everything from node 1, and put it into node 0
-                for wire in node_attachments[1].line_wires:
-                    node_attachments[0].line_wires.add(wire)
-                for dot in node_attachments[1].dot_junctions:
-                    node_attachments[0].dot_junctions.add(dot)
+                intersections[0].merge(intersections[1], wire)
+                self.node_list.remove(intersections[1])
 
-                # Add new wire onto node
-                newWire = Wire(end1, end2)
-                node_attachments[0].add_wire_to_node(newWire)
-                if junctions is True:
-                    node_attachments[0].add_junction(intersections[0])
-                    node_attachments[0].add_junction(intersections[1])
-
-                # Remove it from the self.node_list to avoid "ghost" nodes.
-                for node, i in zip(self.node_list, range(len(self.node_list))):
-                    if node == node_attachments[1]:
-                        self.node_list.remove(node)
-                        break
-
-                # Finally, delete node 1
-                del node_attachments[1]
-
-
-class Wire(VMobject):
-    def __init__(self, end1, end2, **kwargs):
-        super().__init__(**kwargs)
-
-        # Consider using a different approach than using Lines.
-        self.wire = Line(end1, end2)
-        self.add(self.wire)
-        self.node_root = None
-
-    def get_anchors(self):
-        return self.wire.get_anchors()
+            elif len(intersections) == 3:
+                intersections[0].merge(intersections[1], wire)
+                intersections[0].merge(intersections[2])
+                self.node_list.remove(intersections[1])
+                self.node_list.remove(intersections[2])
 
 
 class Node(VMobject):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        # add Wire().wire (or VMobject: Line())
-        # into self.line_wires
-        self.line_wires = VGroup()
-        self.dot_junctions = VGroup()
-
         # NOTE In a future update, I want to try to
         # have designated calculated "voltages"
         # relative to ground using sympy or some python and spice
         # library. Something like pyspice or ngspice...
         # self.known_voltage = None
+        # Voltage functionality has not been added in v2 yet.
+        self.voltage = None
 
-        self.add(self.line_wires)
-        self.add(self.dot_junctions)
+        self.coords = []
+        self.junction_dots = VGroup()
 
-    def add_wire_to_node(self, wire):
-        self.line_wires.add(wire.wire)
-        wire.root = self
+        self.add(self.junction_dots)
 
-    def add_junction(self, coordinate):
-        self.dot_junctions.add(Dot(coordinate))
+        self.set_color(WHITE)
+
+    def __update(self):
+        self.clear_points()
+        for path in self.coords:
+            self.start_new_path(path[0])
+            for coord in path[1:]:
+                self.add_line_to(np.array(coord))
+
+    def check_coord(self, coord):
+        # coord is to be checked.
+        # return a non-False value if:
+        # 1. It paired coordinates in self.coords (and not endpoint)
+        #       return True
+        # 2. It is validated by line_intersection "proof"
+        #       return the coordinate
+        for wire in self.coords:
+            # new wire is connected to an end of a wire.
+            if np.allclose(coord, wire[0]) or np.allclose(coord, wire[-1]):
+                return True
+
+            # reading the endpoints of wire
+            for i in range(len(wire) - 1):
+                coord_is_in_between_a_line = validate_forms_approx_line(
+                    coord, [wire[i], wire[i + 1]]
+                )
+                # Coord matches a coordinate that is in the middle.
+                if (
+                    np.allclose(coord, wire[i]) and i != 0
+                ) or coord_is_in_between_a_line:
+                    return coord
+
+        return False
+
+    def add_dot(self, dot_coord):
+        self.junction_dots.add(Dot(dot_coord))
+
+    # wire is just a matrix with dimensions 2n x 3 or 3 x 3
+    # depending entirely on if it is a diagonal wire or not
+    def add_wire(self, wire_param):
+        if len(self.coords) == 0:
+            self.coords.append(wire_param)
+            self.__update()
+            return
+
+        # Check if continuity in any wire (assume False)
+        cont = False
+        # wire is a single path of arbitrary length. It's a dot chain.
+        for wire, i in zip(self.coords, range(len(self.coords))):
+            if np.allclose(wire[0], wire_param[0]):
+                self.coords[i] = wire_param[1:][::-1] + wire
+                cont = True
+                break
+
+            elif np.allclose(wire[0], wire_param[-1]):
+                self.coords[i] = wire_param[:-1] + wire
+                cont = True
+                break
+
+            elif np.allclose(wire[-1], wire_param[0]):
+                self.coords[i] = wire + wire_param[1:]
+                cont = True
+                break
+
+            elif np.allclose(wire[-1], wire_param[-1]):
+                self.coords[i] = wire + wire_param[:-1][::-1]
+                cont = True
+                break
+
+        # Aggregated data
+        if cont is not True:
+            self.coords.append(wire_param)
+
+        self.__update()
+
+    def merge(self, node, wire=False):
+        if wire is not False:
+            self.add_wire(wire)
+
+        # Get all the junction dots.
+        for dot in node.junction_dots:
+            self.junction_dots.add(dot)
+
+        self.coords = self.coords + node.coords
+        node.clear_points()
+        self.__update()
+
+
+def distance(a, b):
+    return np.sqrt(np.sum([i * i for i in np.array(a) - np.array(b)]))
+
+
+def validate_forms_approx_line(coord, line, tolerance=1e-5):
+    # Check if the sum of the distance(s) between a coordinate to the end(s) of a line
+    # equates to the distance of the line
+    return math.isclose(
+        distance(coord, line[0]) + distance(coord, line[1]),
+        distance(*line),
+        rel_tol=tolerance,
+    )
